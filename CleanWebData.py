853dadd3a4102ff5c606e12e45f3ebe790a7ef37
southeo <15srt2@queensu.ca>
2020-07-06 15:10:35 -0400

    Add files via upload

---
+++ CleanWebData.py
@@ -0,0 +1,4 @@
+# Replace "NA", "NONE", "none", "" with "N/A" where appropriate
+# Consolodate gender/sex information into one column
+# If "disease" is given as "healthy", change to "N/A"
+#tissue consolidation
