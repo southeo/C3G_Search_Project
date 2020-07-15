0983c86c05c014602955633ad5294d528c4df603
southeo <15srt2@queensu.ca>
2020-07-15 09:51:45 -0400

    Add files via upload

---
+++ FinderScript.py
@@ -0,0 +1,152 @@
+import csv
+import glob
+import sys
+import argparse
+import os
+import json
+import copy
+
+INSTANCE_SEARCHES = ["primary_id", "secondary_id", "assay_type", "experiment_type", "archive"]  # these searches are handled differently
+KEYWORD_SEARCHES = ["donor_keyword_id", "disease_keywords", "donor_ethnicity_keywords", "tissue_keywords"]
+AGE_UNITS = ["year", "month", "week", "day"]
+
+def help():
+    print("help info")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-q',
+                        '--query_table',
+                        help="Text file containing search information",
+                        required=True)
+    parser.add_argument('-r',
+                        '--ref_table',
+                        help="Reference table from EBI site",
+                        required=True)
+    return parser.parse_args()
+
+
+def check_args(args):
+    # make sure files exist
+    assert (os.path.isfile(args.query_table)), "Query table not found"
+    assert (os.path.isfile(args.ref_table)), "Reference table file not found"
+
+    # first step: optional, update ref table from ebi website (keep old version)
+    # Second step: search
+
+
+def get_search_list(query_table):
+    # reads query table and collects search functions
+    search_list = []
+    for elem in next(query_table_csv):
+        search_list.append(elem.casefold())
+    #print(search_list)
+    return search_list
+
+
+def match_search_params(scope, query, value, search_list):
+    modified_scope = {"data": []}  # stores potential matches. This will limit the scope as searching progresses
+    print(query, ", ", type(query))
+    for elem in scope["data"]:
+        if query.casefold() in INSTANCE_SEARCHES:
+            bad_matches = len(elem["instances"])
+            # catch all bad matches. Loop through list of instances until certain there are none that sneak past
+            while bad_matches > 0:
+                bad_matches = len(elem["instances"])  #assume all items in this elem are bad
+                for inst in elem["instances"]:
+                    if value != str(inst[query]).casefold():  # remove all bad instance matches
+                        elem["instances"].pop(elem["instances"].index(inst))
+                    else:
+                        bad_matches -= 1  #one less bad match
+            if elem["instances"]:  # if instance list is not empty
+                modified_scope["data"].append(elem)  # Append only the results with the correct instance searches
+        elif query == "age_min" and query in elem.keys():
+                print(value)
+                try:
+                    value = float(value)
+                except ValueError:
+                    print("Invalid min age")
+                if "age_min" in elem.keys() and value <= elem["age_min"]:
+                    modified_scope["data"].append(elem)
+        elif query == "age_max":
+            if query in elem.keys():
+                try:
+                    value = float(value)
+                except ValueError:
+                    print("Invalid max age")
+                if "age_max" in elem.keys() and value >= elem["age_max"]:
+                     modified_scope["data"].append(elem)
+        elif query == "age_exact":
+            if query in elem.keys():
+                try:
+                    value = float(value)
+                except ValueError:
+                    print("Invalid exact age")
+                if "age_exact" in elem.keys() and value == elem["age_exact"]:
+                     modified_scope["data"].append(elem)
+        elif query.casefold() in KEYWORD_SEARCHES:
+            # keyword searches: all items in input must be satisfied, but there may be extra keywords in the search element
+            value_keywords = value.split()
+            keywords_all_match = True
+            for keyword in value_keywords:
+                if keyword not in elem[query]:
+                    keywords_all_match = False
+            if keywords_all_match:
+                modified_scope["data"].append(elem)
+        else:
+            if query in elem.keys() and value == str(elem[query]).casefold():
+                modified_scope["data"].append(elem)
+    return modified_scope
+
+
+def print_results(scope, search_params, row):
+    search_params_shortlist = []
+    instance_flag = False
+    # generate list of search parameters used in this particular query
+    for elem in row:
+        idx = row.index(elem)
+        if elem:
+            search_params_shortlist.append(search_params[idx])
+    match_count = 0
+    #print(search_params_shortlist, instance_flag)
+    if scope["data"]:
+        for elem in scope["data"]:
+            print(elem)
+            print("\n \n IHEC ID: ", elem["IHEC ID"])
+            for param in search_params_shortlist:
+                match_count += 1
+                if param not in INSTANCE_SEARCHES:
+                    print('\t', param, ":", elem[param], end="")
+                else:
+                    print("\n {  ", param, ": \n \t", end="")
+                    for inst in elem["instances"]:
+                        print(inst[param], ", ", end="")
+                    print("\n }")
+    print("\n Number of matches: ", match_count)
+
+args = parse_args()
+check_args(args)
+with open(args.query_table) as qt, open(args.ref_table) as rt:
+    query_table_csv = csv.reader(qt, delimiter='\t')
+    ref_table_json = json.load(rt)
+    search_list = get_search_list(query_table_csv)
+    search_list_copy = copy.deepcopy(search_list)
+
+
+    for row in query_table_csv:
+        scope = copy.deepcopy(ref_table_json)  # reset scope for each search
+        search_list = copy.deepcopy(search_list_copy)  # reset search list (values have been popped)
+        for val in row:
+            try:
+                search_param = search_list.pop(0)
+            except IndexError:
+                print("Column Missmatch. Please ensure all rows in the search "
+                      "file have the same number of columns")
+            val_to_match = val.casefold()
+            if search_param == "donor_age" or search_param == "age_min" or search_param == "age_max":
+                val_to_match = val_to_match.split(",")  # take age as a list with 3 properties: val, unit, flag
+            print(val_to_match)
+            if val_to_match:  # if string is not empty -> ie it is a valid search parameter
+                scope = match_search_params(scope, search_param, val_to_match, search_list_copy)
+        print_results(scope, search_list_copy, row)
---
+++ WebScraper.py
@@ -0,0 +1,299 @@
+# General import
+import shutil
+from urllib.request import urlopen
+from collections import defaultdict
+from multiprocessing import Pool, Manager
+import requests
+import json
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+from lxml import html
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+from shutil import copyfile
+import os
+from datetime import date
+import math
+
+IHEC_PORTAL_URL = "https://www.ebi.ac.uk/vg/epirr/view/"  # must be cat'd with "all" or IHECRE ID
+remove_char_list = [',', '.', ';', '(', ')']
+
+
+def requests_retry_session(retries=3, backoff_factor=0.3, status_forcelist=(500, 502, 504), session=None, ):
+    """Retry timeout requests"""
+    session = session or requests.Session()
+    retry = Retry(total=retries, read=retries, connect=retries, backoff_factor=backoff_factor,
+                  status_forcelist=status_forcelist, )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
+
+
+def parse_ihec_page(url):
+    # Parses single web page based on url (which includes IHEC_RE ID)
+
+    metadata_dict = dict()
+    rawdata_dict = dict()
+
+    ihec_response = requests_retry_session().get(url)
+    if ihec_response.ok:
+        soup = BeautifulSoup(ihec_response.content, 'html.parser')  # object that parses html
+
+        # Metadata: This will be the same for each item associated with a given IHEC ID. Create single dictionary
+        # each entry can pull from
+        ihec_id = soup.find("h1")  # IHEC ID
+        dd_list = soup.find_all("dd")  #
+        dt_list = soup.find_all("dt")
+
+        count = 0
+        metadata_dict['ihec_id'] = ihec_id.string
+        while count < len(dd_list):
+            metadata_dict[str(dt_list[count].string).casefold()] = str(dd_list[count].string).casefold()
+            count += 1
+        metadata_dict['instances'] = []  # this will be filled in in next step
+
+        # Raw Data: This will be different for each item. Create dictionary that will be unique for each entry.
+        # Not that metadata changes from one IHEC ID to another, but this data has the same format
+        table = soup.find('table')
+        table_rows = table.find_all('tr')
+        for tr in table_rows:
+            rawdata_dict = ()  # reset vals -> necessary in python?
+            td = tr.find_all('td')
+            if len(td) != 0:  # make sure there is data
+                metadata_dict['instances'].append({
+                    "primary_id": td[3].string,
+                    "secondary_id": td[4].string,
+                    "assay_type": td[0].string,
+                    "experiment_type": td[1].string,
+                    "archive": td[2].string,
+                })
+        return metadata_dict
+
+
+def parse_ihec_db():
+    # parses entire EBI database
+    ihec_response = requests_retry_session().get(IHEC_PORTAL_URL + "all")
+    if ihec_response.ok:
+        soup = BeautifulSoup(ihec_response.content, 'html.parser')
+        ebi_dict = {'data': []}
+        id_list = []
+        # put all ihec ids in a list:
+        for row in soup.find_all('table')[0].tbody.find_all('tr'):
+            id_list.append(row.find_all('td')[3].contents)
+        for ihec_id in id_list:
+            ebi_dict['data'].append(parse_ihec_page(IHEC_PORTAL_URL + ihec_id[0]))
+            print(ihec_id[0], " Complete")
+
+        # write raw results
+        with open('EBI_Database_Raw.txt', 'w') as outfile:
+            json.dump(ebi_dict, outfile, indent=4)
+
+
+
+def consolidate_tissue(data_file):
+    with open(data_file, 'r') as database_file:
+        db_json = json.load(database_file)
+        for elem in db_json["data"]:
+            tissue_keywords = []
+            if "tissue_type" in elem and elem["tissue_type"] is not None:
+                tissue_keywords += elem["tissue_type"].split()
+            if "tissue-type" in elem and elem["tissue-type"] is not None:
+                tissue_keywords += elem["tissue-type"].split()
+            if "tissue" in elem and elem["tissue"] is not None:
+                tissue_keywords += elem["tissue"].split()
+            if "origin_sample" in elem and elem["origin_sample"] is not None:
+                tissue_keywords += elem["origin_sample"].split()
+            if "cell ontology" in elem and elem["cell ontology"] is not None:
+                tissue_keywords += elem["cell ontology"].split()
+            if "lineage" in elem and elem["lineage"] is not None:
+                tissue_keywords += elem["lineage"].split()
+            if "cell_type" in elem and elem["cell_type"] is not None:
+                tissue_keywords += elem["cell_type"].split()
+            if "histological_type" in elem and elem["histological_type"] is not None:
+                tissue_keywords += elem["histological_type"].split()
+
+            elem["tissue_keywords"] = tissue_keywords
+
+    with open(data_file, 'w') as outfile:
+        json.dump(db_json, outfile, indent=4)
+
+
+def consolidate_gender(data_file):
+    with open(data_file, 'r') as database_file:
+        db_json = json.load(database_file)
+        for elem in db_json["data"]:
+            if "gender" not in elem:
+                if "donor_sex" in elem and elem["donor_sex"] is not None:
+                    elem["gender"] = elem["donor_sex"]
+                elif "sex" in elem and elem["sex"] is not None:
+                    elem["gender"] = elem["sex"]
+
+    with open(data_file, 'w') as outfile:
+        json.dump(db_json, outfile, indent=4)
+
+
+def consolidate_ethnicity(data_file):
+    with open(data_file, 'r') as database_file:
+        db_json = json.load(database_file)
+        for elem in db_json['data']:
+            ethnicity_keywords = []
+            if "population" in elem and elem["population"] is not None:
+                ethnicity_keywords += elem["population"].split()
+            if "ethnicity" in elem and elem["ethnicity"] is not None:
+                ethnicity_keywords += elem["ethnicity"].split()
+            if "donor_ethnicity" in elem and elem["donor_ethnicity"] is not None:
+                ethnicity_keywords += elem['donor_ethnicity'].split()
+
+            # make searching a little easier:
+            if "white" in ethnicity_keywords or "White" in ethnicity_keywords:
+                ethnicity_keywords.append("Caucasian")
+            elif "Caucasian" in ethnicity_keywords:
+                ethnicity_keywords.append("White")
+            if "African" in ethnicity_keywords and "American" in ethnicity_keywords or "AA" in ethnicity_keywords:
+                ethnicity_keywords.extend(["Black", "African American"])
+            if "Native" in ethnicity_keywords and "American" in ethnicity_keywords:
+                ethnicity_keywords.extend(["Native American", "First Nations", "Indigenous"])
+            elem['donor_ethnicity_keywords'] = ethnicity_keywords
+
+    with open(data_file, 'w') as outfile:
+        json.dump(db_json, outfile, indent=4)
+
+'''
+def consolidate_age(data_file):
+    with open(data_file, 'r') as database_file:
+        db_json = json.load(database_file)
+        for elem in db_json['data']:
+            if "donor_life_stage" in elem and elem["donor_life_stage"] is not None:
+                if elem["donor_life_stage"] == "embryonic" or elem["donor_life_stage"] == "fetal":
+                    elem["prenatal"] = True
+                else:
+                    elem["prenatal"] = False
+            else:
+                elem["prenatal"] = False
+
+    with open(data_file, 'w') as outfile:
+        json.dump(db_json, outfile, indent=4)
+'''
+
+def consolidate_donor_id(data_file):
+    with open(data_file, 'r') as database_file:
+        db_json = json.load(database_file)
+        for elem in db_json["data"]:
+            id_keywords = []
+            if "donor id" in elem and elem["donor id"] is not None:
+                id_keywords.append(elem["donor id"])
+            if "subject_id" in elem and elem["subject_id"] is not None:
+                id_keywords.append(elem["subject_id"])
+            if "sample_id" in elem and elem["sample_id"] is not None:
+                id_keywords.append(elem["sample_id"])
+            if "donor_id" in elem and elem["donor_id"] is not None:
+                id_keywords.append(elem["donor_id"])
+            elem["donor_keyword_id"] = id_keywords
+
+    with open(data_file, 'w') as outfile:
+        json.dump(db_json, outfile, indent=4)
+
+def is_float(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
+def age_in_years(age_list, divisor):
+    new_age_list = []
+    for val in age_list:
+        new_age_list.append((math.trunc(val*100/divisor)/100))
+    return new_age_list
+
+
+def consolidate_age(data_file):
+    with open(data_file, 'r') as database_file:
+        db_json = json.load(database_file)
+
+        for elem in db_json["data"]:
+            if "age" in elem.keys():
+                char_list = elem["age"].split()
+            elif "donor_age" in elem.keys():
+                char_list = elem["donor_age"].split()
+            else:
+                char_list = []
+            if char_list:
+                float_list = []
+                while len(char_list) != 0:
+                    next_char = char_list.pop()  #clear out the characters, keep the numbers
+                    if is_float(next_char):
+                        float_list.append(float(next_char))
+                if float_list:
+                    if "donor_age_unit" in elem.keys():
+                        #print("before: ", float_list)
+                        if elem["donor_age_unit"] == "day":
+                            float_list = age_in_years(float_list, 365)
+                        elif elem["donor_age_unit"] == "week":
+                            float_list = age_in_years(float_list, 52)
+                        elif elem["donor_age_unit"] == "month":
+                            float_list = age_in_years(float_list, 12)
+                        #print("after: ", float_list)
+                    elem["age_min"] = min(float_list)  # First element in the age list will be value
+                    elem["age_max"] = max(float_list)
+                    if elem["age_min"] == elem["age_max"]:
+                        elem["age_exact"] = elem["age_min"]
+                    else:
+                        elem["age_exact"] = ("")     # if there is a range this key does not contain useful info
+            if "donor_life_stage" in elem.keys() and \
+                    (elem["donor_life_stage"] == "fetal" or elem["donor_life_stage"] == "embryonic"):
+                elem["prenatal"] = True
+            else:
+                elem["prenatal"] = False
+
+    with open(data_file, 'w') as outfile:
+        json.dump(db_json, outfile, indent=4)
+
+
+def consolidate_disease(data_file):
+    with open(data_file, 'r') as database_file:
+        db_json = json.load(database_file)
+
+        for elem in db_json["data"]:
+            disease_list = []
+            if "disease" in elem and elem["disease"] is not None:
+                # remove some formatting issues
+                elem["disease"] = elem["disease"].replace(",", "")
+                elem["disease"] = elem["disease"].replace(";", "")
+                elem["disease"] = elem["disease"].replace("(", "")
+                elem["disease"] = elem["disease"].replace(")", "")
+                elem["disease"] = elem["disease"].replace("_", " ")
+                # add each term to keywords list
+                disease_list = elem["disease"].split()
+            if "donor_health_status" in elem and elem["donor_health_status"] is not None:
+                disease_list += elem["donor_health_status"].split()
+
+            elem["disease_keywords"] = disease_list
+
+    with open(data_file, 'w') as outfile:
+        json.dump(db_json, outfile, indent=4)
+
+
+def consolidate_all(data_file):
+    home_dir = os.getcwd() + '\\' + data_file
+    raw_dir = os.getcwd() + '\Raw_DB\EBI_Database_' + str(date.today()) + ".txt"
+    consolidated_file = "EBI_Database_Consolidated_" + str(date.today()) + ".txt"
+    if not os.path.isfile(consolidated_file):
+        shutil.copy2(home_dir, raw_dir)
+        os.rename(data_file, consolidated_file)
+        # consolidate data
+        consolidate_age(consolidated_file)
+        consolidate_disease(consolidated_file)
+        consolidate_donor_id(consolidated_file)
+        consolidate_ethnicity(consolidated_file)
+        consolidate_gender(consolidated_file)
+        consolidate_tissue(consolidated_file)
+        consolidate_age(consolidated_file)
+
+
+
+#parse_ihec_db()
+consolidate_age("EBI_Database_Consolidated_2020-07-09.txt")
+
