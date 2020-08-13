32a27ce08e43462bf7fa30de3b37accea710df0a
southeo <15srt2@queensu.ca>
2020-08-13 12:11:07 -0400

    debug time what is elem?

--- data_organization.py
+++ data_organization.py
@@ -187,6 +187,7 @@ def scan_through(ref_list, dest_dir):
     rejected_extensions = []
     missing_list = []
     for elem in os.listdir():
+        print(type(elem))
         if os.path.isfile(elem) and is_datafile(elem):
             misc_id, missing_list = fetch_id(elem, missing_list)  # get the EGAX/etc id from the filename or the onsite list
             if misc_id:  # if there is a match for secondary id
