3f4e2f9ca083fa790d2d1f7701ae86b3661f09a6
southeo <15srt2@queensu.ca>
2020-08-15 18:30:55 -0400

    path creation

--- data_organization.py
+++ data_organization.py
@@ -87,12 +87,12 @@ def scan_through(ref_list):  # Scans through source directory and moves stuff ar
             if misc_id:  # if there is a match for secondary id
                 ihec_ids = match_to_db(misc_id, ref_list)  # list of ihec ids in which this file appears
                 earliest_id = ihec_ids.pop(0)
-                file_path = DEST_DIR + "/" + str(earliest_id[0:14]) #+ "/" + str(earliest_id)
+                file_path = os.path.join(DEST_DIR, str(earliest_id[0:14])) #+ "/" + str(earliest_id)
                 print(file_path)
                 if not os.path.exists(file_path):  # if path does not already exist
-                    os.mkdirs(file_path)
-                    file_path = file_path + "/" + str(earliest_id)
                     os.mkdir(file_path)
+                    #file_path = os.path.join(file_path, str(earliest_id))
+                    #os.mkdir(file_path)
                 # shutil.move(str(os.getcwd()+elem), path)  # Uncomment when ready to move files
 
                 # make symlinks for the rest of the occurrences:
