9ff76f3fd326c56e189eb01ca771919d53600134
southeo <15srt2@queensu.ca>
2020-08-16 07:10:05 -0400

    misc_id fixing

--- data_organization.py
+++ data_organization.py
@@ -58,7 +58,7 @@ def fetch_id(filename, missing_list):
             break
     #print("First Check: ", retval, "Filename: ", filename)
     if not retval:  # if retval is empty
-        onsite_list = pd.read_csv(ON_SITE_TABLE)
+        onsite_list = csv.reader(ON_SITE_TABLE)
         next(onsite_list)
         for row in onsite_list:
             fn = row[4]  # where the file name is stored
