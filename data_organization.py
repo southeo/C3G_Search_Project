10d58c257a15e598d8abd31fb2a4c44fb52cccba
Soulaine <15srt2@queensu.ca>
2020-08-16 22:42:13 -0400

    match ihec ids

--- data_organization.py
+++ data_organization.py
@@ -164,7 +164,7 @@ def match_to_db(misc_id, ref_list):
     ihec_ids = []
     for elem in ref_list["data"]:
         for inst in elem["instances"]:
-            if inst["primary_id"] == misc_id or inst["secondary_id"] or \
+            if inst["primary_id"] == misc_id or inst["secondary_id"] == misc_id or \
                     ("egar_id" in inst.keys() and misc_id in inst['egar_id']) or \
                     ("egaf_id" in inst.keys() and misc_id in inst['egaf_id']):
                 print("Misc id", misc_id)
