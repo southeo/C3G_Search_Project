6a4a4bc64e7ad8abbef1d8ddd5f48847bd920c18
Soulaine <15srt2@queensu.ca>
2020-08-17 10:15:49 -0400

    move list formatting

--- data_organization.py
+++ data_organization.py
@@ -176,5 +176,4 @@ with open(REF_TABLE) as rt, open("Move_List.txt", 'w') as mv_lst:
     ref_table = json.load(rt)
     move_list = []
     move_list = scan_through(ref_table, move_list)
-    print(move_list)
     json.dump(move_list, mv_lst, indent=2)
