7424b3cf452c89483487f19247138c8b2616f1ac
Soulaine <15srt2@queensu.ca>
2020-08-18 11:49:07 -0400

    parse xml

--- data_organization.py
+++ data_organization.py
@@ -62,12 +62,13 @@ def get_JGAR_id(dir_name):
     md_file = os.path.join(JGAD_DIR, md_file)
     tree = ET.parse(md_file)
     root = tree.getroot()
+    '''
     for child in root:
         for refname in child.iter("refname"):
             print(refname)
-
-#    for refname in root.iter("refname"):
-#        print(refname)
+'''
+    for refname in root.iter("refname"):
+        print(refname)
 
 
     '''
