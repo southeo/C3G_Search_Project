8f3b56363e0f9796bd6f266886e85dcf8cbe6b8d
Soulaine <15srt2@queensu.ca>
2020-08-18 11:33:09 -0400

    parse xml

--- data_organization.py
+++ data_organization.py
@@ -64,7 +64,7 @@ def get_JGAR_id(dir_name):
     root = tree.getroot()
     for child in root:
         for granchild in child:
-            print(granchild.tag)
+            print(granchild.tag, "...", granchild.attrib)
 
     '''
     blah blah blah parse JGAD tree and return its 
