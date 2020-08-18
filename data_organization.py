21ee94fda0c9c5cdd3d85b8230ec1817d5b01fd0
Soulaine <15srt2@queensu.ca>
2020-08-18 11:11:55 -0400

    parse xml

--- data_organization.py
+++ data_organization.py
@@ -62,6 +62,7 @@ def get_JGAR_id(dir_name):
     md_file = os.path.join(JGAD_DIR, md_file)
     tree = ET.parse(md_file)
     root = tree.getroot()
+    print(root[0])
     for alias in root[0].iter("alias"):
         print(alias.attrib)
     '''
