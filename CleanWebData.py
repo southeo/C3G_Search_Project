200ac98cf3ea68e2f572bba0cbb948641869c45b
southeo <15srt2@queensu.ca>
2020-08-20 09:09:32 -0400

    Delete CleanWebData.py

--- CleanWebData.py
+++
@@ -1,4 +0,0 @@
-# Replace "NA", "NONE", "none", "" with "N/A" where appropriate
-# Consolodate gender/sex information into one column
-# If "disease" is given as "healthy", change to "N/A"
-#tissue consolidation
