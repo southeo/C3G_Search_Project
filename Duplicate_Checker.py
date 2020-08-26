4c28c5c16f9cef52f852f34d14105154bca2faba
Soulaine <15srt2@queensu.ca>
2020-08-26 10:32:44 -0400

    get egaf id

--- Duplicate_Checker.py
+++ Duplicate_Checker.py
@@ -1,5 +1,6 @@
 import csv
 import hashlib
+import os
 
 def hash_bytestr_iter(bytesiter, hasher, ashexstr=False):
     for block in bytesiter:
@@ -14,6 +15,7 @@ def file_as_blockiter(afile, blocksize=65536):
             yield block
             block = afile.read(blocksize)
 
+
 def is_same_hash(path1, path2):
     hash1 = hash_bytestr_iter(file_as_blockiter(open(path1, 'rb')), hashlib.sha256())
     hash2 = hash_bytestr_iter(file_as_blockiter(open(path2, 'rb')), hashlib.sha256())
@@ -25,7 +27,7 @@ with open("Duplicate_list_all.txt") as dup, open("False_dups.txt", "r+") as fd:
     dup_reader = csv.reader(dup, delimiter=',')
     writer = csv.writer(fd)
     for row in dup_reader:
-        file1 = row[1]
+        file1 = os.path.join(row[1], row[0])
         file2 = row[2]
         if not is_same_hash(file1, file2):
             new_row = [file1, file2]
