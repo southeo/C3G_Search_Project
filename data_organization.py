483fd69271216d716f3df95c2945b65d1544fc6c
Soulaine <15srt2@queensu.ca>
2020-08-26 21:25:36 -0400

    link local ids to metadata file

--- data_organization.py
+++ data_organization.py
@@ -232,7 +232,6 @@ def move_files(ihec_ids, elem, move_list, misc_id):
 def get_sub_dir(misc_id, ref_list):
     for elem in ref_list["data"]:
         for inst in elem["instances"]:
-            print(inst)
             if misc_id in inst["primary_id"] \
                     or (inst["secondary_id"] is not None and misc_id in inst["secondary_id"]) \
                     or ("egaf" in inst.keys() and misc_id in inst["egaf"]) \
