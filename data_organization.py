40a3051ad7d8ce8bf1fbc5cf3dee79aa58e14bd6
Soulaine <15srt2@queensu.ca>
2020-08-28 14:01:21 -0400

    upodate filename in mdf

--- data_organization.py
+++ data_organization.py
@@ -366,7 +366,7 @@ def update_filename(ref_list, filename, misc_id):
                     inst["filename"] = [filename]
                 pp.pprint(inst)
 
-    json.dump(ref_list)
+    json.dump(ref_list, ref_list, indent=4)
 
 def is_datafile(filename):
     for ext in ACCEPTED_EXTENSIONS:
