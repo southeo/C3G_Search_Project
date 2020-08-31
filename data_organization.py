cc31d7780db511e8dbcc790d98f3903780bb2ee7
Soulaine <15srt2@queensu.ca>
2020-08-31 14:46:41 -0400

    search testy

--- data_organization.py
+++ data_organization.py
@@ -353,7 +353,7 @@ def update_filename(ref_list, filename, misc_id):
                 else:
                     inst["filename"] = [(filename, misc_id)]
 
-    with open(REF_TABLE) as rt:
+    with open(REF_TABLE, 'w') as rt:
         json.dump(ref_list, rt, indent=4)
 
 
