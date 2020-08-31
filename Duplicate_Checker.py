0534ad390aabc268b22b06f346d08d65599c8e1e
Soulaine <15srt2@queensu.ca>
2020-08-30 22:33:10 -0400

    duplicate checker parallel, fix args

--- Duplicate_Checker.py
+++ Duplicate_Checker.py
@@ -21,7 +21,6 @@ def is_same_hash(path1, path2):
     hash1 = hash_bytestr_iter(file_as_blockiter(open(path1, 'rb')), hashlib.sha256())
     hash2 = hash_bytestr_iter(file_as_blockiter(open(path2, 'rb')), hashlib.sha256())
     filename = path2.split('/')[-1]
-    dup_reader = csv.reader(dup, delimiter=',')
     if hash1 == hash2:
         file = path1.split('/')[-1]
         new_row = [file, path1, path2]
