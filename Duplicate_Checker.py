3ae4694b4574e04b660563602419de20d758c9a4
Soulaine <15srt2@queensu.ca>
2020-08-30 22:16:00 -0400

    duplicate checker parallel

--- Duplicate_Checker.py
+++ Duplicate_Checker.py
@@ -1,6 +1,7 @@
 import csv
 import hashlib
 import os
+import multiprocessing as mp
 
 def hash_bytestr_iter(bytesiter, hasher, ashexstr=False):
     for block in bytesiter:
@@ -20,23 +21,30 @@ def is_same_hash(path1, path2):
     hash1 = hash_bytestr_iter(file_as_blockiter(open(path1, 'rb')), hashlib.sha256())
     hash2 = hash_bytestr_iter(file_as_blockiter(open(path2, 'rb')), hashlib.sha256())
     filename = path2.split('/')[-1]
-    print("Hash is same:", hash1 == hash2, '\t', filename)
-    return hash1 == hash2
-
-
-
-with open("Duplicate_list_all.txt") as dup, open("False_dups_time_check.txt", "r+") as fd:
-    dup_reader = csv.reader(dup, delimiter=',')
-    writer = csv.writer(fd)
-    for row in dup_reader:
-        file1 = os.path.join(row[1], row[0])
-        file2 = row[2]
-        if not is_same_hash(file1, file2):
-            new_row = [file1, file2]
+    with open("False_dups_parallel", 'r+') as fd:
+        dup_reader = csv.reader(dup, delimiter=',')
+        writer = csv.writer(fd)
+        if hash1 != hash2:
+            file = path1.split('/')[-1]
+            new_row = [file, path1, path2]
             writer.writerow(new_row)
-            print("ROW ADDED: ", row)
+
+def setup_func(row0, row1, row2):
+    return os.path.join(row1, row0), row2
 
 
 
+with open("Duplicate_list_all.txt") as dup, open(mp.Pool(mp.cpu_count())) as pool:
+    pool = mp.Pool(mp.cpu_count())
+    output = mp.Queue()
+    processes = [mp.Process(is_same_hash(setup_func(row[0], row[1], row[2]))) for row in dup]
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    results = [output.get() for p in processes]
 
 
