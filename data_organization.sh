1312450f3a500f0c0ff1f9b5d24546da3f860432
Soulaine <15srt2@queensu.ca>
2020-08-31 12:49:08 -0400

    search testy

--- data_organization.sh
+++ data_organization.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --time=18:00:00
 #SBATCH --account=def-bourqueg
-chmod +x Move_List_with_egaf.txt
-chmod +x Duplicate_list_all.txt
-python data_organization.py -r $PWD -d ../../ihec_data_struct/ -s ../../../jbodpool_mp2/
+chmod +x Move_List_EGAD00001002674.txt
+chmod +x Duplicate_list_EGAD00001002674.txt
+python data_organization.py -d ../../Epigenomic_Data_Home/ -s ../../../jbodpool_mp2/EpiVar/EGAD00001002674 -r $PWD -m True
