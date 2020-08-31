492cb2e3cae20bc02dd9856c5931908c128db62a
Soulaine <15srt2@queensu.ca>
2020-08-30 22:21:46 -0400

    duplicate checker parallel

--- Duplicate_Checker.py
+++ Duplicate_Checker.py
@@ -21,23 +21,20 @@ def is_same_hash(path1, path2):
     hash1 = hash_bytestr_iter(file_as_blockiter(open(path1, 'rb')), hashlib.sha256())
     hash2 = hash_bytestr_iter(file_as_blockiter(open(path2, 'rb')), hashlib.sha256())
     filename = path2.split('/')[-1]
-    with open("False_dups_parallel", 'r+') as fd:
-        dup_reader = csv.reader(dup, delimiter=',')
-        writer = csv.writer(fd)
-        if hash1 != hash2:
-            file = path1.split('/')[-1]
-            new_row = [file, path1, path2]
-            writer.writerow(new_row)
+    dup_reader = csv.reader(dup, delimiter=',')
+    if hash1 != hash2:
+        file = path1.split('/')[-1]
+        new_row = [file, path1, path2]
+        output.put(new_row)
 
 def setup_func(row0, row1, row2):
     return os.path.join(row1, row0), row2
 
+with open("Duplicate_list_all.txt") as dup:
 
-
-with open("Duplicate_list_all.txt") as dup, open(mp.Pool(mp.cpu_count())) as pool:
     pool = mp.Pool(mp.cpu_count())
     output = mp.Queue()
-    processes = [mp.Process(is_same_hash(setup_func(row[0], row[1], row[2]))) for row in dup]
+    processes = [mp.Process(target=is_same_hash(), args=(setup_func(row[0], row[1], row[2]))) for row in dup]
 
     for p in processes:
         p.start()
@@ -46,5 +43,6 @@ with open("Duplicate_list_all.txt") as dup, open(mp.Pool(mp.cpu_count())) as poo
         p.join()
 
     results = [output.get() for p in processes]
+    print(results)
 
 
