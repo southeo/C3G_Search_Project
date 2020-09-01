4bf33999a3b18b57addc6acdbe96201e6232f0b1
Soulaine <15srt2@queensu.ca>
2020-09-01 13:03:04 -0400

    finder testy

--- FinderScript.py
+++ FinderScript.py
@@ -170,11 +170,10 @@ def get_location(scope, search_list, val_list):
                     elem["ihec_id"]  # get path to where the file SHOULD be...
         if path.exists(ihec_path):
             #for inst in elem["instances"]:  # Cycle through instances of each match
-            for file in os.listdir(ihec_path):
-                print(file)
             for filename in os.listdir(ihec_path):  # Cycle through files in directory
                 inst = match_files(filename, elem)
                 if inst:
+                    print(inst)
                     if elem["ihec_id"] not in ihec_list:
                         if "read1" in str(filename):
                             results["data"].append({
