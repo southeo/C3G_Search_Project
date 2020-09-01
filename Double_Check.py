10cc3420e5a0f6f6978369cd3d8e511c91de5bb7
Soulaine <15srt2@queensu.ca>
2020-09-01 14:27:49 -0400

    verify finder

--- Double_Check.py
+++ Double_Check.py
@@ -23,7 +23,7 @@ with open("EBI_Database_Consolidated_2020-08-31.txt", 'r') as rt, open("Matches.
                     else:
                         missed_files.append(file)
 
-    print("Search Results: ", len(set(file_list)), ", Good Matches: ", len(set(good_files)), ", Missed files:", len(missed_files))
+    print("Search Results: ", len(set(file_list)), ", Good Matches: ", len(set(good_files)), ", Missed files:", len(set(missed_files)))
 
 
 
