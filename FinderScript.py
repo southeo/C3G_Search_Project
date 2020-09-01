724589ddc4462bd84799d4b4a656d87082d54a26
Soulaine <15srt2@queensu.ca>
2020-09-01 13:01:23 -0400

    finder testy

--- FinderScript.py
+++ FinderScript.py
@@ -170,7 +170,8 @@ def get_location(scope, search_list, val_list):
                     elem["ihec_id"]  # get path to where the file SHOULD be...
         if path.exists(ihec_path):
             #for inst in elem["instances"]:  # Cycle through instances of each match
-            print(os.listdir(ihec_path))
+            for file in os.listdir(ihec_path):
+                print(file)
             for filename in os.listdir(ihec_path):  # Cycle through files in directory
                 inst = match_files(filename, elem)
                 if inst:
