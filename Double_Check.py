d3380c5f92e53aa8557ead297fb803547adc0dda
Soulaine <15srt2@queensu.ca>
2020-09-01 14:19:54 -0400

    verify finder

--- Double_Check.py
+++ Double_Check.py
@@ -14,9 +14,7 @@ with open("EBI_Database_Consolidated_2020-08-31.txt", 'r') as rt, open("Matches.
 
     for elem in ref_table_json["data"]:
         for inst in elem["instances"]:
-            print(inst['assay_type'])
             if (inst['assay_type']).casefold() == ("RNA-Seq").casefold() and "filename" in inst.keys():
-                print((inst["filename"]).casefold())
                 for file in inst["filename"]:
                     if file in file_list:
                         good_files.append(file)
