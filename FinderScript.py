ac34d5ec6c4c22aa9edf30f88566e54620352483
Soulaine <15srt2@queensu.ca>
2020-09-01 12:57:32 -0400

    finder testy

--- FinderScript.py
+++ FinderScript.py
@@ -211,7 +211,7 @@ def get_location(scope, search_list, val_list):
                                     res["r2_path"] = (str(ihec_path) + "/" + str(filename))
                                     print("r2")
                                     #res = {k: res[k] for k in ordered_keys}
-                                print(res.keys())
+                                print(res)
 
     return results
 
