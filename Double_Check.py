41290a70dedd2b6a66cc3c279a14171203ef2c26
Soulaine <15srt2@queensu.ca>
2020-09-01 14:11:57 -0400

    verify finder

--- Double_Check.py
+++ Double_Check.py
@@ -10,7 +10,7 @@ with open("EBI_Database_Consolidated_2020-08-31.txt", 'r') as rt, open("Matches.
     for elem in matches:
         for data in matches["data"]:
             print(data.keys())
-            #file_list.append(data["filename"])
+            file_list.append(data["filename"])
 
 
     for elem in ref_table_json["data"]:
