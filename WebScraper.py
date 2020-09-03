8884641b01b260595dfa1218755f2080843f125d
Soulaine <15srt2@queensu.ca>
2020-09-03 13:27:26 -0400

    consolidate experiments

--- WebScraper.py
+++ WebScraper.py
@@ -278,7 +278,7 @@ def consolidate_experiment(data_file):
 
                 if "Histone" in exp: exp = "ChIP-Seq " + exp
 
-                db_json["experiment_type"] = exp.casefold()
+                inst["experiment_type"] = exp.casefold()
 
     with open(data_file, 'w') as outfile:
         json.dump(db_json, outfile, indent=4)
